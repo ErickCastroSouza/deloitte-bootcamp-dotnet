fa25f5d6ac6aa38623afcad607de13d52379c3f3
aluno <aluno@embarquedigital.portodigital.local>
2026-01-29 14:26:54 -0300

    Add dia-0 and dia-01 as normal directories

---
+++ dia-01/Case1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace BootcampCase1;
+
+class Program
+{
+    static string path = @"C:\Users\aluno\Desktop\bootcamp_ddt\Case1\bootcamp-deloitte-case-1\";
+    static string fileName = "users.txt";
+    static string filePath = path + fileName;
+
+    static void Main()
+    {
+        Console.WriteLine("============= Cadastro de informações =============");
+
+        CriarArquivo();
+
+        string nome = ExibirMensagemNome();
+        int idade = ExibirMensagemIdade();
+
+        string linha = $"{nome};{idade}";
+        File.AppendAllText(filePath, linha + Environment.NewLine);
+
+        Console.WriteLine("Usuário salvo com sucesso!");
+    }
+
+    static string ExibirMensagemNome()
+    {
+        Console.WriteLine("1 - Digite seu nome completo:");
+        string nome = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(nome) || nome.Length < 2)
+        {
+            Console.WriteLine("O nome não pode estar vazio ou ter menos de 2 caracteres. Tente novamente.");
+            return ExibirMensagemNome();
+        }
+
+        return nome;
+    }
+
+    static int ExibirMensagemIdade()
+    {
+        Console.WriteLine("2 - Digite sua idade:");
+        string idade = Console.ReadLine();
+
+        if (!int.TryParse(idade, out int idadeInt) || idadeInt <= 0)
+        {
+            Console.WriteLine("Idade inválida. Tente novamente.");
+            return ExibirMensagemIdade();
+        }
+
+        return idadeInt;
+    }
+
+    static void CriarArquivo()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath).Close();
+        }
+    }
+}
---
+++ dia-01/Case1/README.md
@@ -0,0 +1,3 @@
+# bootcamp-deloitte-case-1
+
+antes de rodar, edite o path para se adequar á pasta local onde está o projeto!
