f5b964039b18237b9f1ddd8f8633d9be44ec590c
aluno <aluno@embarquedigital.portodigital.local>
2026-02-04 15:44:40 -0300

    adding dia-06

--- dia-01/api-piloto/obj/Debug/net10.0/api-piloto.AssemblyInfo.cs
+++ dia-01/api-piloto/obj/Debug/net10.0/api-piloto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-piloto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f842de8965d20ccbbef0f6286e2e3895ee9f6f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14eafc34a044ba844b23e25abc21f89883583d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-piloto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-piloto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
